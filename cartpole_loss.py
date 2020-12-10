fix normalization and restructure

cartpole_loss.py
@@ -73,11 +73,11 @@ def control_loss_function(action, state, lambda_factor=.4, printout=0):
         state = state_to_theta(state, action[:, i])
     abs_state = torch.abs(state)
 
-    pos_loss = state[:, 0]**2
+    pos_loss = abs_state[:, 0]
     # velocity losss is low when x is high
-    vel_loss = .1 * abs_state[:, 1] * (2.4 - abs_state[:, 0])**2
+    vel_loss = abs_state[:, 1] * (2.4 - abs_state[:, 0])**2
     angle_loss = abs_state[:, 2] + .1 * abs_state[:, 3]
-    loss = .1 * (pos_loss + vel_loss + 2 * angle_loss)
+    loss = (pos_loss + vel_loss + 2 * angle_loss)
     # .1 * torch.mv(abs_state, weighting)  # * prev_weighted
     # print(state**2)
     # execute with the maximum force

dataset.py
@@ -4,7 +4,9 @@ import os
 from environments.cartpole_env import construct_states
 
 
-def raw_states_to_torch(states, normalize=False, std=None, return_std=False):
+def raw_states_to_torch(
+    states, normalize=False, std=None, mean=None, return_std=False
+):
     """
     Helper function to convert numpy state array to normalized tensors
     Argument states:
@@ -19,16 +21,20 @@ def raw_states_to_torch(states, normalize=False, std=None, return_std=False):
         # can't use mean!
         if std is None:
             std = np.std(states, axis=0)
-        states = states / std
+        if mean is None:
+            mean = np.mean(states, axis=0)
+        states = (states - mean) / std
         # assert np.all(np.isclose(np.std(states, axis=0), 1))
     else:
         std = 1
+
+    # np.save("data_backup/quad_data.npy", states)
 
     states_to_torch = torch.from_numpy(states).float()
 
     # if we computed mean and std here, return it
     if return_std:
-        return states_to_torch, std
+        return states_to_torch, mean, std
     return states_to_torch
 
 
@@ -37,15 +43,17 @@ class Dataset(torch.utils.data.Dataset):
     def __init__(
         self,
         state_sampling_method,
-        path_to_states=None,
         num_states=1000,
         normalize=False,
+        mean=None,
         std=None
     ):
         self.std = std
-        # random_positions = np.random.rand(1000, 3) * 10
+        self.mean = mean
+        # sample states
         state_arr_numpy = state_sampling_method(num_states)
-        state_arr, self.std = raw_states_to_torch(
+        # convert to normalized tensors
+        state_arr, self.mean, self.std = raw_states_to_torch(
             state_arr_numpy, normalize=normalize, std=std, return_std=True
         )
         self.labels = state_arr

environments/drone_env.py
@@ -61,7 +61,7 @@ class QuadRotorEnvBase(gym.Env):
     @staticmethod
     def get_is_stable(np_state):
         roll_pitch_yaw = np_state[3:6]
-        return np.sum(np.absolute(roll_pitch_yaw)) < 1
+        return np.sum(np.absolute(roll_pitch_yaw)) < .9
 
     def step(self, action):
         action = np.clip(self._process_action(action), 0.0, 1.0)
@@ -102,12 +102,17 @@ class QuadRotorEnvBase(gym.Env):
     def reset(self):
 
         self._state = DynamicsState()
+        self.randomize_angle(5)
 
-        self.randomize_angle(20)
         self.randomize_angular_velocity(2.0)
         self._state.attitude.yaw = self.random_state.uniform(
             low=-0.3, high=0.3
         )
+        # print(
+        #     "angle after randomize:",
+        #     abs(self._state.attitude.roll) + abs(self._state.attitude.pitch) +
+        #     abs(self._state.attitude.yaw)
+        # )
         self._state.position[2] = 1
         # yaw control typically expects slower velocities
         self._state.angular_velocity[2] *= 0.5
@@ -251,7 +256,6 @@ def construct_states(num_data, episode_length=15):
             data.append(new_state)
             time += 1
     data = np.array(data)
-    # np.save("data_backup/quad_data.npy", data)
     return data
 
 

evaluate_drone.py
@@ -12,11 +12,13 @@ from models.resnet_like_model import Net
 
 class QuadEvaluator():
 
-    def __init__(self, model, std):
+    def __init__(self, model, mean=0, std=1):
+        self.mean = mean
         self.std = std
         self.net = model
 
     def stabilize(self, nr_iters=1, render=False, max_time=100):
+        actions = []
         with torch.no_grad():
             eval_env = QuadRotorEnvBase()
             collect_runs = list()
@@ -26,19 +28,27 @@ class QuadEvaluator():
                 current_np_state = eval_env._state.as_np
                 stable = True
                 while stable and time_stable < max_time:
-                    current_torch_state = torch.from_numpy(
-                        np.array([current_np_state])
-                    ).float()
+                    current_torch_state = raw_states_to_torch(
+                        current_np_state,
+                        normalize=True,
+                        mean=self.mean,
+                        std=self.std
+                    )
                     suggested_action = self.net(current_torch_state)
                     suggested_action = torch.sigmoid(suggested_action
                                                      )[0].numpy()
+                    actions.append(suggested_action)
+                    if render:
+                        print("action:", np.around(suggested_action, 2))
                     current_np_state, stable = eval_env.step(suggested_action)
                     if render:
                         eval_env.render()
                         time.sleep(.1)
                     time_stable += 1
                 collect_runs.append(time_stable)
-        return np.mean(collect_runs), np.std(collect_runs)
+        act = np.array(actions)
+        print("avg and std action", np.mean(act, axis=0), np.std(act, axis=0))
+        return np.mean(collect_runs), np.std(collect_runs),
 
 
 if __name__ == "__main__":
@@ -62,12 +72,16 @@ if __name__ == "__main__":
     model_path = os.path.join("trained_models", "drone", model_name)
 
     # load std or other parameters from json
-    with open(os.path.join(model_path, "std.json"), "r") as outfile:
-        std = np.array(json.load(outfile)["std"])
+    with open(os.path.join(model_path, "param_dict.json"), "r") as outfile:
+        param_dict = json.load(outfile)
 
     net = torch.load(os.path.join(model_path, "model_quad"))
     net.eval()
 
-    evaluator = QuadEvaluator(net, std)
-    success_mean, success_std = evaluator.stabilize(nr_iters=3, render=True)
+    evaluator = QuadEvaluator(
+        net,
+        mean=np.array(param_dict["mean"]),
+        std=np.array(param_dict["std"])
+    )
+    success_mean, success_std = evaluator.stabilize(nr_iters=1, render=True)
     print(success_mean, success_std)

train_cartpole.py
@@ -8,7 +8,7 @@ from dataset import Dataset
 from cartpole_loss import control_loss_function
 from evaluate_cartpole import Evaluator
 from models.resnet_like_model import Net
-from utils.plotting import plot_loss
+from utils.plotting import plot_loss, plot_success
 from environments.cartpole_env import construct_states
 
 NR_EVAL_ITERS = 10
@@ -91,41 +91,10 @@ for epoch in range(NR_EPOCHS):
 
 # PLOTTING
 SAVE_PATH = "trained_models/minimize_x"
-episode_length_mean = np.array(episode_length_mean)
-episode_length_std = np.array(episode_length_std)
-plt.figure(figsize=(20, 10))
-x = np.arange(len(episode_length_mean))
-plt.plot(x, episode_length_mean, '-')
-plt.fill_between(
-    x,
-    episode_length_mean - episode_length_std,
-    episode_length_mean + episode_length_std,
-    alpha=0.2
-)
-plt.xlabel("Epoch", fontsize=18)
-plt.ylabel("Average episode length", fontsize=18)
-plt.xticks(fontsize=18)
-plt.yticks(fontsize=18)
-plt.savefig(os.path.join(SAVE_PATH, "performance.png"))
-
-plot_loss(loss_list, SAVE_PATH)
-
-# pole_angle_mean = np.array(pole_angle_mean)
-# pole_angle_std = np.array(pole_angle_std)
-# plt.figure(figsize=(20, 10))
-# plt.plot(x, pole_angle_mean, '-')
-# plt.fill_between(
-#     x,
-#     pole_angle_mean - pole_angle_std,
-#     pole_angle_mean + pole_angle_std,
-#     alpha=0.2
-# )
-# plt.xlabel("Epoch", fontsize=18)
-# plt.xticks(fontsize=18)
-# plt.yticks(fontsize=18)
-# plt.ylabel("Pole angles", fontsize=18)
-# plt.savefig("models/pole_angles.png")
 
 # SAVE MODEL
 torch.save(net, os.path.join(SAVE_PATH, "model_pendulum"))
+# PLOT
+plot_loss(loss_list, SAVE_PATH)
+plot_success(episode_length_mean, episode_length_std, SAVE_PATH)
 print('Finished Training')

train_drone.py
@@ -8,65 +8,93 @@ from drone_loss import drone_loss_function
 from evaluate_drone import QuadEvaluator
 from models.resnet_like_model import Net
 from environments.drone_env import construct_states
-from utils.plotting import plot_loss
+from utils.plotting import plot_loss, plot_success
 
-EPOCH_SIZE = 10000
+EPOCH_SIZE = 1000
 PRINT = (EPOCH_SIZE // 30)
-NR_EPOCHS = 10
+NR_EPOCHS = 50
 BATCH_SIZE = 8
 NR_EVAL_ITERS = 10
 
 net = Net(20, 4)
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+# TESTING
+# reference_data = Dataset(construct_states, normalize=True, num_states=1000)
+# trainloader = torch.utils.data.DataLoader(
+#     reference_data, batch_size=20, shuffle=True, num_workers=0
+# )
+# for i, data in enumerate(trainloader, 0):
+#     inputs, current_state = data
+#     optimizer.zero_grad()
+#     action = net(inputs)
+#     action = torch.sigmoid(action)
+#     print(action)
+# print(fail)
+
 reference_data = Dataset(
     construct_states, normalize=True, num_states=EPOCH_SIZE
 )
-STD = reference_data.std
+(STD, MEAN) = (reference_data.std, reference_data.mean)
 
-loss_list = list()
+loss_list, success_mean_list, success_std_list = list(), list(), list()
 
 for epoch in range(NR_EPOCHS):
 
     # Generate data dynamically
     state_data = Dataset(
-        construct_states, normalize=True, std=STD, num_states=EPOCH_SIZE
+        construct_states,
+        normalize=True,
+        mean=MEAN,
+        std=STD,
+        num_states=EPOCH_SIZE
     )
     trainloader = torch.utils.data.DataLoader(
         state_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
     )
 
-    eval_env = QuadEvaluator(net, STD)
+    eval_env = QuadEvaluator(net, MEAN, STD)
     suc_mean, suc_std = eval_env.stabilize(nr_iters=NR_EVAL_ITERS)
+    success_mean_list.append(suc_mean)
+    success_std_list.append(suc_std)
     print(f"Epoch {epoch}: Time: {round(suc_mean, 1)} ({round(suc_std, 1)})")
 
     running_loss = 0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, current_state = data
+    try:
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, current_state = data
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # forward + backward + optimize
-        actions = net(inputs)
-        actions = torch.sigmoid(actions)
-        lam = epoch / NR_EPOCHS
-        loss = drone_loss_function(current_state, actions)
-        loss.backward()
-        optimizer.step()
+            # forward
+            actions = net(inputs)
+            actions = torch.sigmoid(actions)
 
-        # print statistics
-        running_loss += loss.item()
-        if i % PRINT == PRINT - 1:
-            print('Loss: %.3f' % (running_loss / PRINT))
-            loss_list.append(running_loss / PRINT)
-            running_loss = 0.0
+            # compute loss + backward + optimize
+            loss = drone_loss_function(current_state, actions)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % PRINT == PRINT - 1:
+                print('Loss: %.3f' % (running_loss / PRINT))
+                loss_list.append(running_loss / PRINT)
+                running_loss = 0.0
+    except KeyboardInterrupt:
+        break
 
 SAVE = os.path.join("trained_models/drone/test_model")
 if not os.path.exists(SAVE):
     os.makedirs(SAVE)
-with open(os.path.join(SAVE, "std.json"), "w") as outfile:
-    json.dump({"std": STD.tolist()}, outfile)
-plot_loss(loss_list, SAVE)
+# save std for normalization
+param_dict = {"std": STD.tolist(), "mean": MEAN.tolist()}
+with open(os.path.join(SAVE, "param_dict.json"), "w") as outfile:
+    json.dump(param_dict, outfile)
+#
 torch.save(net, os.path.join(SAVE, "model_quad"))
+plot_loss(loss_list, SAVE)
+plot_success(success_mean_list, success_std_list, SAVE)
+print("finished and saved.")