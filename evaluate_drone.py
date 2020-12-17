fix normalization, add self play

evaluate_drone.py
@@ -22,6 +22,7 @@ class QuadEvaluator():
         self.net = model
 
     def stabilize(self, nr_iters=1, render=False, max_time=200):
+        collect_data = []
         actions = []
         failure_list = list()  # collect the reason for failure
         collect_runs = list()
@@ -66,6 +67,7 @@ class QuadEvaluator():
                         #     # print(np.around(current_np_state[3:6], 2))
                         #     print("action:", np.around(suggested_action, 2))
                         current_np_state, stable = eval_env.step(action)
+                        collect_data.append(current_np_state)
                         if not stable:
                             att_stable, pos_stable = eval_env.get_is_stable(
                                 current_np_state
@@ -83,13 +85,16 @@ class QuadEvaluator():
                         time.sleep(.1)
                 collect_runs.append(time_stable)
         act = np.array(actions)
+        collect_data = np.array(collect_data)
         print(
             "Position was responsible in ", round(np.mean(failure_list), 2),
             "cases"
         )
         print("avg and std action", np.mean(act, axis=0), np.std(act, axis=0))
-        return np.mean(collect_runs), np.std(collect_runs
-                                             ), np.mean(failure_list)
+        return (
+            np.mean(collect_runs), np.std(collect_runs), np.mean(failure_list),
+            collect_data
+        )
 
 
 if __name__ == "__main__":
@@ -130,7 +135,7 @@ if __name__ == "__main__":
     # watch
     evaluator.stabilize(nr_iters=1, render=True)
     # compute stats
-    success_mean, success_std, _ = evaluator.stabilize(
+    success_mean, success_std, _, _ = evaluator.stabilize(
         nr_iters=100, render=False
     )
     print(success_mean, success_std)

train_drone.py
@@ -1,5 +1,6 @@
 import os
 import json
+import numpy as np
 import torch.optim as optim
 import torch
 import torch.nn.functional as F
@@ -12,6 +13,7 @@ from environments.drone_env import construct_states
 from utils.plotting import plot_loss, plot_success
 
 EPOCH_SIZE = 10000
+USE_NEW_DATA = 0  # 1000
 PRINT = (EPOCH_SIZE // 30)
 NR_EPOCHS = 200
 BATCH_SIZE = 8
@@ -24,23 +26,15 @@ SAVE = os.path.join("trained_models/drone/test_model")
 net = Net(STATE_SIZE, NR_ACTIONS * ACTION_DIM)
 optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
-# TESTING
-# reference_data = Dataset(construct_states, normalize=True, num_states=100)
-# trainloader = torch.utils.data.DataLoader(
-#     reference_data, batch_size=8, shuffle=True, num_workers=0
-# )
-# for i, data in enumerate(trainloader, 0):
-#     inputs, current_state = data
-#     optimizer.zero_grad()
-#     action = net(inputs)
-#     action = torch.sigmoid(action)
-#     print(action)
-# print(fail)
-
 reference_data = Dataset(
     construct_states, normalize=True, num_states=EPOCH_SIZE
 )
 (STD, MEAN) = (reference_data.std, reference_data.mean)
+
+# save std for normalization
+param_dict = {"std": STD.tolist(), "mean": MEAN.tolist()}
+with open(os.path.join(SAVE, "param_dict.json"), "w") as outfile:
+    json.dump(param_dict, outfile)
 
 loss_list, success_mean_list, success_std_list = list(), list(), list()
 
@@ -53,14 +47,15 @@ for epoch in range(NR_EPOCHS):
         normalize=True,
         mean=MEAN,
         std=STD,
-        num_states=EPOCH_SIZE
+        num_states=EPOCH_SIZE,
+        # reset_strength=.6 + epoch / 50
     )
     trainloader = torch.utils.data.DataLoader(
         state_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
     )
 
     eval_env = QuadEvaluator(net, MEAN, STD)
-    suc_mean, suc_std, pos_responsible = eval_env.stabilize(
+    suc_mean, suc_std, pos_responsible, new_data = eval_env.stabilize(
         nr_iters=NR_EVAL_ITERS
     )
     if epoch > 2 and suc_mean > highest_success:
@@ -71,6 +66,12 @@ for epoch in range(NR_EPOCHS):
     success_mean_list.append(suc_mean)
     success_std_list.append(suc_std)
     print(f"Epoch {epoch}: Time: {round(suc_mean, 1)} ({round(suc_std, 1)})")
+
+    # self-play: add acquired data
+    if USE_NEW_DATA > 0 and epoch > 2:
+        rand_inds_include = np.random.permutation(len(new_data))[:USE_NEW_DATA]
+        state_data.add_data(np.array(new_data)[rand_inds_include])
+        print("newly acquired data:", new_data.shape, state_data.states.size())
 
     running_loss = 0
     try:
@@ -112,10 +113,7 @@ for epoch in range(NR_EPOCHS):
 
 if not os.path.exists(SAVE):
     os.makedirs(SAVE)
-# save std for normalization
-param_dict = {"std": STD.tolist(), "mean": MEAN.tolist()}
-with open(os.path.join(SAVE, "param_dict.json"), "w") as outfile:
-    json.dump(param_dict, outfile)
+
 #
 torch.save(net, os.path.join(SAVE, "model_quad"))
 plot_loss(loss_list, SAVE)