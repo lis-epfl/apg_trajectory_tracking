fbc86695bea81599072661e152ef860356f4e1a0
NinaWie <wnina@student.ethz.ch>
2020-12-26 21:59:52 +0100

    rename layers

--- models/hutter_model.py
+++ models/hutter_model.py
@@ -8,12 +8,16 @@ class Net(nn.Module):
     def __init__(self, in_size, out_size):
         super(Net, self).__init__()
         # conf: in channels, out channels, kernel size
-        self.fc1 = nn.Linear(in_size, 64)
+        self.fc_in = nn.Linear(in_size, 64)
+        self.fc1 = nn.Linear(64, 64)
         self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, out_size)
+        self.fc3 = nn.Linear(64, 64)
+        self.fc_out = nn.Linear(64, out_size)
 
     def forward(self, x):
+        x = torch.tanh(self.fc_in(x))
         x = torch.tanh(self.fc1(x))
-        x = torch.tanh(self.fc2(x))
-        x = self.fc3(x)
+        # x = torch.tanh(self.fc2(x))
+        # x = torch.tanh(self.fc3(x))
+        x = self.fc_out(x)
         return x
