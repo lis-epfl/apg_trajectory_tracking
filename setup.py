e61369d64f8de531aa74639b9bcb3be1188848e2
NinaWie <ninawiedemann999@gmail.com>
2021-02-17 10:56:08 +0100

    add ruamel dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='neural_control',
     version='0.0.1',
-    install_requires=['torch', 'numpy', 'matplotlib', 'scipy', 'pyglet'],
+    install_requires=['torch', 'numpy', 'matplotlib', 'scipy', 'pyglet', 'ruamel.yaml'],
     packages=find_packages()
 )
