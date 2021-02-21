0da0051115666ee47db72cbbb31109d8ce969a60
NinaWie <ninawiedemann999@gmail.com>
2021-02-21 14:30:32 +0100

    change eval script to compare several models

--- eval_script.py
+++ eval_script.py
@@ -12,106 +12,112 @@ except ModuleNotFoundError:
 
 eval_dict = {
     "straight": {
-        "nr_test": 20,
+        "nr_test": 0,
         "max_steps": 1000
     },
     "circle": {
-        "nr_test": 20,
+        "nr_test": 0,
         "max_steps": 1000
     },
     "poly": {
-        "nr_test": 20,
+        "nr_test": 50,
         "max_steps": 1000
     }
 }
-thresh_stable = 1
+thresh_stable = 1.5
 thresh_divergence = 3
 
 if __name__ == "__main__":
-    model_name = "current_model"
-    epoch = ""
-    out_path = "outputs"
-    save_model_name = "flightmare"
-    use_flightmare = True
+    models_to_evaluate = ["current_model", "horizon_1", "motion_prior"]
+    names = ["normal", "larger step size", "motion prior"]
 
-    df = pd.DataFrame(
-        columns=[
-            "Model", "max_drone_dist", "Trajectory", "Circle plane",
-            "Circle radius", "Circle direction", "Stable steps",
-            "Tracking error", "Speed", "Diverged"
-        ]
-    )
+    for model_name, save_name in zip(models_to_evaluate, names):
+        for use_flightmare in [0,1]:
+            env_name = "flightmare" if use_flightmare else "simple env"
+            print(f"---------------- {model_name} in env {env_name} --------")
 
-    model_path = os.path.join("trained_models", "drone", model_name)
+            # model_name = "current_model"
+            epoch = ""
+            out_path = "outputs"
+            save_model_name = save_name + f" ({env_name})"
+            print("save_model_name", save_model_name)
+            # use_flightmare = True
 
-    net, param_dict = load_model(model_path, epoch=epoch)
-
-
-    model_name = save_model_name
-
-    dt = 0.05
-    for max_drone_dist in [0.5, 0.75]:
-        print("---------------------------------")
-        param_dict["dt"] = dt
-        param_dict["max_drone_dist"] = max_drone_dist
-        horizon = 10
-
-        dataset = DroneDataset(1, 1, **param_dict)
-        evaluator = QuadEvaluator(
-            net,
-            dataset,
-            take_every_x=5000,
-            render=0,
-            **param_dict
-        )
-        if use_flightmare:
-            evaluator.eval_env = FlightmareWrapper(
-                    param_dict["dt"], False
+            df = pd.DataFrame(
+                columns=[
+                    "Model", "max_drone_dist", "Trajectory", "Circle plane",
+                    "Circle radius", "Circle direction", "Stable steps",
+                    "Tracking error", "Speed", "Diverged"
+                ]
             )
 
-        for reference, ref_params in eval_dict.items():
-            # run x times
-            for _ in range(ref_params["nr_test"]):
-                # define circle specifications
-                if reference == "circle":
-                    circle_args = evaluator.sample_circle()
-                    # if use_flightmare:
-                    #     circle_args["plane"] = [0,1]
-                else:
-                    circle_args = {"plane": 0, "radius": 0, "direction": 0}
+            model_path = os.path.join("trained_models", "drone", model_name)
 
-                # run trajectory tracking
-                _, drone_ref, divergence = evaluator.follow_trajectory(
-                    reference,
-                    max_nr_steps=ref_params["max_steps"],
-                    thresh_stable=thresh_stable,
-                    thresh_div=thresh_divergence,
-                    **circle_args
+            net, param_dict = load_model(model_path, epoch=epoch)
+
+
+            dt = 0.05
+            for max_drone_dist in [0.25, 0.5]:
+                param_dict["dt"] = dt
+                param_dict["max_drone_dist"] = max_drone_dist
+                horizon = 10
+
+                dataset = DroneDataset(1, 1, **param_dict)
+                evaluator = QuadEvaluator(
+                    net,
+                    dataset,
+                    take_every_x=5000,
+                    render=0,
+                    **param_dict
                 )
-                # compute results
-                avg_divergence = np.mean(divergence)
-                steps_until_fail = len(drone_ref)
-                if reference == "poly" and len(drone_ref) > 500:
-                    drone_ref = drone_ref[100:-500]
-                try:
-                    speed = evaluator.compute_speed(drone_ref)
-                except ZeroDivisionError:
-                    speed = np.nan
+                if use_flightmare:
+                    evaluator.eval_env = FlightmareWrapper(
+                            param_dict["dt"], False
+                    )
 
-                was_diverged = int(steps_until_fail < ref_params["max_steps"]
-                        and divergence[-1] > thresh_divergence)
+                for reference, ref_params in eval_dict.items():
+                    # run x times
+                    for _ in range(ref_params["nr_test"]):
+                        # define circle specifications
+                        if reference == "circle":
+                            circle_args = evaluator.sample_circle()
+                            # if use_flightmare:
+                            #     circle_args["plane"] = [0,1]
+                        else:
+                            circle_args = {"plane": 0, "radius": 0, "direction": 0}
 
-                # log
-                print(
-                    reference, "len", steps_until_fail, "div", avg_divergence,
-                    "speed", speed, "diverged?", was_diverged
-                )
-                df.loc[len(df)] = [
-                    model_name, max_drone_dist, reference,
-                    circle_args["plane"], circle_args["radius"],
-                    circle_args["direction"], steps_until_fail, avg_divergence,
-                    speed, was_diverged
-                ]
+                        # run trajectory tracking
+                        _, drone_ref, divergence = evaluator.follow_trajectory(
+                            reference,
+                            max_nr_steps=ref_params["max_steps"],
+                            thresh_stable=thresh_stable,
+                            thresh_div=thresh_divergence,
+                            **circle_args
+                        )
+                        # compute results
+                        avg_divergence = np.mean(divergence)
+                        steps_until_fail = len(drone_ref)
+                        if reference == "poly" and len(drone_ref) > 500:
+                            drone_ref = drone_ref[100:-500]
+                        try:
+                            speed = evaluator.compute_speed(drone_ref)
+                        except ZeroDivisionError:
+                            speed = np.nan
 
-    print(df)
-    df.to_csv(os.path.join(out_path, f"eval_{model_name}.csv"))
+                        was_diverged = int(steps_until_fail < ref_params["max_steps"]
+                                and divergence[-1] > thresh_divergence)
+
+                        # log
+                        print(
+                            reference, "len", steps_until_fail, "div", avg_divergence,
+                            "speed", speed, "diverged?", was_diverged
+                        )
+                        df.loc[len(df)] = [
+                            save_model_name, max_drone_dist, reference,
+                            circle_args["plane"], circle_args["radius"],
+                            circle_args["direction"], steps_until_fail, avg_divergence,
+                            speed, was_diverged
+                        ]
+
+            print(df)
+            df.to_csv(os.path.join(out_path, f"eval_{save_model_name}.csv"))
