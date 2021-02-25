44f1cd7a78ff024abaf1f3ed0f706e98a853912d
NinaWie <wnina@student.ethz.ch>
2021-02-26 00:47:26 +0100

    wip: added fixed wing dynamics to mpc

--- neural_control/controllers/mpc.py
+++ neural_control/controllers/mpc.py
@@ -6,6 +6,8 @@ import numpy as np
 import time
 from os import system
 
+# import parameters
+# from neural_control.environments.wing_longitudinal_dynamics import long_dynamics
 from neural_control.environments.copter import copter_params
 from types import SimpleNamespace
 
@@ -87,6 +89,10 @@ class MPC(object):
             # initial state and control action TODO
             self._quad_s0 = (np.zeros(12) + .5).tolist()
             self._quad_u0 = (np.zeros(4) + .5).tolist()
+        elif self.dynamics_model == "fixed_wing":
+            self._Q_pen = np.diag([10, 10, 0, 0, 0, 0])
+            self._quad_s0 = np.array([0, 0, 10, 0, 0, 0])
+            self._quad_u0 = (np.zeros(4) + .5).tolist()
 
         # cost matrix for the action
         self._Q_u = np.diag([0.1, 0.1, 0.1, 0.1])  # T, wx, wy, wz
@@ -103,6 +109,8 @@ class MPC(object):
             F = self.drone_dynamics_high_mpc(self._dt)
         elif self.dynamics_model == "simple_quad":
             F = self.drone_dynamics_simple(self._dt)
+        elif self.dynamics_model == "fixed_wing":
+            F = self.fixed_wing_dynamics(self.dt)
         fMap = F.map(self._N, "openmp")  # parallel
 
         # # # # # # # # # # # # # # #
@@ -199,6 +207,8 @@ class MPC(object):
                 delta_u_k = U[:, k] - [.5, .5, .5, .5]
             elif self.dynamics_model == "high_mpc":
                 delta_u_k = U[:, k] - [self._gz, 0, 0, 0]
+            elif self.dynamics_model == "fixed_wing":
+                delta_u_k = U[:, k] - [.5, .5]
 
             cost_u_k = f_cost_u(delta_u_k)
 
@@ -465,5 +475,99 @@ class MPC(object):
             vz_new, avx_new, avy_new, avz_new
         )
         # Fold
+        F = ca.Function('F', [self._x, self._u], [X])
+        return F
+
+    def fixed_wing_dynamics(self, dt):
+        """
+        Longitudinal dynamics for fixed wing
+        """
+
+        # ------------------ constants ---------------------
+        m = 1.01
+        I_xx = 0.04766
+        rho = 1.225
+        S = 0.276
+        c = 0.185
+        g = 9.81
+        # linearized for alpha = 0 and u = 12 m/s
+        Cl0 = 0.3900
+        Cl_alpha = 4.5321
+        Cl_q = 0.3180
+        Cl_del_e = 0.527
+        # drag coefficients
+        Cd0 = 0.0765
+        Cd_alpha = 0.3346
+        Cd_q = 0.354
+        Cd_del_e = 0.004
+        # moment coefficients
+        Cm0 = 0.0200
+        Cm_alpha = -1.4037
+        Cm_q = -0.1324
+        Cm_del_e = -0.4236
+
+        # --------- state vector ----------------
+        px, pz, vx, vz, theta, q = (
+            ca.SX.sym('px'), ca.SX.sym('pz'), ca.SX.sym('vx'), ca.SX.sym('vz'),
+            ca.SX.sym('theta'), ca.SX.sym('q')
+        )
+        self._x = ca.vertcat(px, py, vx, vz, theta, q)
+
+        # -----------control command ---------------
+        thrust, del_e = ca.SX.sym('thrust'), ca.SX.sym('del_e')
+        self._u = ca.vertcat(thrust, del_e)
+
+        x = px  # x position
+        h = pz  # z position / altitude
+        u = vx  # forward velocity in body frame
+        w = vz  # upward velocity  in body frame
+
+        # input states
+        T = 1.3 + thrust * .4 - .2
+        del_e = np.pi * (del_e * 10 - 5) / 180
+
+        ## aerodynamic forces calculations
+        # (see beard & mclain, 2012, p. 44 ff)
+        V = ca.sqrt(u**2 + w**2)  # velocity norm
+        alpha = ca.atan(w / u)  # angle of attack
+        # alpha = torch.clamp(alpha, -alpha_bound, alpha_bound) TODO
+
+        # NOTE: usually all of Cl, Cd, Cm,... depend on alpha, q, delta_e
+        # lift coefficient
+        Cl = Cl0 + Cl_alpha * alpha + Cl_q * c / (2 * V) * q + Cl_del_e * del_e
+        # drag coefficient
+        Cd = Cd0 + Cd_alpha * alpha + Cd_q * c / (2 * V) * q + Cd_del_e * del_e
+        # pitch moment coefficient
+        Cm = Cm0 + Cm_alpha * alpha + Cm_q * c / (2 * V) * q + Cm_del_e * del_e
+
+        # resulting forces and moment
+        L = 1 / 2 * rho * V**2 * S * Cl  # lift
+        D = 1 / 2 * rho * V**2 * S * Cd  # drag
+        M = 1 / 2 * rho * V**2 * S * c * Cm  # pitch moment
+
+        ## Global displacement
+        x_dot = u * ca.cos(theta) + w * ca.sin(theta)  # forward
+        h_dot = u * ca.sin(theta) + w * ca.cos(theta)  # upward
+
+        ## Body fixed accelerations
+        u_dot = -w * q + (1 / m) * (
+            T + L * ca.sin(alpha) - D * ca.cos(alpha) - m * g * ca.sin(theta)
+        )
+        w_dot = u * q - (1 / m) * (
+            L * ca.cos(alpha) - D * ca.sin(alpha) - m * g * ca.cos(theta)
+        )
+
+        ## Pitch acceleration
+        q_dot = M / I_xx
+
+        x_new = px + dt * x_dot
+        h_new = pz + dt * h_dot
+        u_new = vx + dt * u_dot
+        w_new = vz + dt * w_dot
+        theta_new = theta + dt * q
+        q_new = q + dt * q_dot
+
+        X = ca.vertcat(x_new, h_new, u_new, w_new, theta_new, q_new)
+
         F = ca.Function('F', [self._x, self._u], [X])
         return F
