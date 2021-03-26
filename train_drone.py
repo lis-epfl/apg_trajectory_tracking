177cb4bb725400b02db3203a41ae6d9157934cac
NinaWie <wnina@student.ethz.ch>
2021-03-26 17:49:26 +0100

    major refactor of training to have training class

--- train_drone.py
+++ train_drone.py
@@ -15,6 +15,7 @@ from neural_control.environments.drone_dynamics import SimpleDynamics
 from neural_control.environments.flightmare_dynamics import (
     FlightmareDynamics
 )
+from neural_control.environments.learnt_dynamics import LearntDynamics
 from neural_control.controllers.network_wrapper import NetworkWrapper
 from neural_control.environments.drone_env import QuadRotorEnvBase
 from evaluate_drone import QuadEvaluator
@@ -27,214 +28,286 @@ try:
 except ModuleNotFoundError:
     pass
 
-DYNAMICS = "flightmare"
-DELTA_T = 0.1
-EPOCH_SIZE = 500
-SELF_PLAY = 1.5
-SELF_PLAY_EVERY_X = 2
-PRINT = (EPOCH_SIZE // 30)
-NR_EPOCHS = 200
-BATCH_SIZE = 8
-RESET_STRENGTH = 1.2
-MAX_DRONE_DIST = 0.25
-THRESH_DIV_START = .1
-THRESH_DIV_END = 2
-THRESH_STABLE = 1.5
-USE_MPC_EVERY = 500
-NR_EVAL_ITERS = 5
-STATE_SIZE = 12
-NR_ACTIONS = 10
-REF_DIM = 9
-ACTION_DIM = 4
-LEARNING_RATE = 0.0001
-SPEED_FACTOR = .6
-MAX_STEPS = 1000
-SAVE = os.path.join("trained_models/drone/test_model")
-BASE_MODEL = "trained_models/drone/baseline_flightmare"
-BASE_MODEL_NAME = 'model_quad'
 
-TrainDynamics = (
-    FlightmareDynamics() if DYNAMICS == "flightmare" else SimpleDynamics()
-)
-EvalDynamics = TrainDynamics
+class TrainDrone:
+    """
+    Train a controller for a quadrotor
+    """
 
-# FINE TUNING:
-# THRESH_DIV_START = 1
-# SELF_PLAY = 1.5
-# EPOCH_SIZE = 500
-# MAX_STEPS = 1000
-# SELF_PLAY_EVERY_X = 5
-# LEARNING_RATE = 0.0001
-# EvalDynamics = FlightmareDynamics(modified_params={"down_drag": .75})
+    def __init__(self):
+        self.dynamics = "flightmare"
+        self.delta_t = 0.1
+        self.epoch_size = 500
+        self.self_play = 1
+        self.self_play_every_x = 2
+        self.batch_size = 8
+        self.reset_strength = 1.2
+        self.max_drone_dist = 0.25
+        self.thresh_div_start = .1
+        self.thresh_div_end = 2
+        self.thresh_stable = 1.5
+        self.use_mpc_every = 500
+        self.state_size = 12
+        self.nr_actions = 10
+        self.ref_dim = 9
+        self.action_dim = 4
+        self.learning_rate_controller = 0.0001
+        self.learning_rate_dynamics = 0.1
+        self.speed_factor = .6
+        self.max_steps = 1000
+        self.save_path = os.path.join("trained_models/drone/test_model")
 
-if not os.path.exists(SAVE):
-    os.makedirs(SAVE)
+        # initialize as the original flightmare environment
+        self.train_dynamics = LearntDynamics()
+        # FINE TUNING:
+        # self.thresh_div_start = 1
+        # self.self_play = 1.5
+        # self.epoch_size = 500
+        # self.max_steps = 1000
+        # self.self_play_every_x = 5
+        # self.learning_rate = 0.0001
+        self.eval_dynamics = FlightmareDynamics(
+            modified_params={"down_drag": .75}
+        )
 
-# Load model or initialize model
-if BASE_MODEL is not None:
-    net = torch.load(os.path.join(BASE_MODEL, BASE_MODEL_NAME))
-    # load std or other parameters from json
-    with open(os.path.join(BASE_MODEL, "param_dict.json"), "r") as outfile:
-        param_dict = json.load(outfile)
-    STD = np.array(param_dict["std"]).astype(float)
-    MEAN = np.array(param_dict["mean"]).astype(float)
-else:
-    state_data = DroneDataset(
-        EPOCH_SIZE,
-        SELF_PLAY,
-        reset_strength=RESET_STRENGTH,
-        max_drone_dist=MAX_DRONE_DIST,
-        ref_length=NR_ACTIONS,
-        dt=DELTA_T
-    )
-    in_state_size = state_data.normed_states.size()[1]
-    # +9 because adding 12 things but deleting position (3)
-    net = Net(
-        in_state_size, NR_ACTIONS, REF_DIM, ACTION_DIM * NR_ACTIONS, conv=1
-    )
-    (STD, MEAN) = (state_data.std, state_data.mean)
+        self.count_finetune_data = 0
 
-# Use cuda if available
-global device
-device = "cpu"  # torch.device("cuda" if torch.cuda.is_available() else "cpu")
-net = net.to(device)
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
 
-# define optimizer and torch normalization parameters
-optimizer = optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=0.9)
+    def initialize_model(self, base_model=None, base_model_name="model_quad"):
+        # Load model or initialize model
+        if base_model is not None:
+            self.net = torch.load(os.path.join(base_model, base_model_name))
+            # load std or other parameters from json
+            with open(
+                os.path.join(base_model, "param_dict.json"), "r"
+            ) as outfile:
+                self.param_dict = json.load(outfile)
+            STD = np.array(self.param_dict["std"]).astype(float)
+            MEAN = np.array(self.param_dict["mean"]).astype(float)
+        else:
+            self.state_data = DroneDataset(
+                self.epoch_size,
+                self.self_play,
+                reset_strength=self.reset_strength,
+                max_drone_dist=self.max_drone_dist,
+                ref_length=self.nr_actions,
+                dt=self.delta_t
+            )
+            in_state_size = self.state_data.normed_states.size()[1]
+            # +9 because adding 12 things but deleting position (3)
+            self.net = Net(
+                in_state_size,
+                self.nr_actions,
+                self.ref_dim,
+                self.action_dim * self.nr_actions,
+                conv=1
+            )
+            (STD, MEAN) = (self.state_data.std, self.state_data.mean)
 
-torch_mean, torch_std = (
-    torch.from_numpy(MEAN).float(), torch.from_numpy(STD).float()
-)
+        # save std for normalization during test time
+        self.param_dict = {"std": STD.tolist(), "mean": MEAN.tolist()}
+        # update the used parameters:
+        self.param_dict["reset_strength"] = self.reset_strength
+        self.param_dict["max_drone_dist"] = self.max_drone_dist
+        self.param_dict["horizon"] = self.nr_actions
+        self.param_dict["ref_length"] = self.nr_actions
+        self.param_dict["thresh_div"] = self.thresh_div_start
+        self.param_dict["dt"] = self.delta_t
+        self.param_dict["take_every_x"] = self.self_play_every_x
+        self.param_dict["thresh_stable"] = self.thresh_stable
+        self.param_dict["speed_factor"] = self.speed_factor
 
-# save std for normalization during test time
-param_dict = {"std": STD.tolist(), "mean": MEAN.tolist()}
-# update the used parameters:
-param_dict["reset_strength"] = RESET_STRENGTH
-param_dict["max_drone_dist"] = MAX_DRONE_DIST
-param_dict["horizon"] = NR_ACTIONS
-param_dict["ref_length"] = NR_ACTIONS
-param_dict["thresh_div"] = THRESH_DIV_START
-param_dict["dt"] = DELTA_T
-param_dict["take_every_x"] = SELF_PLAY_EVERY_X
-param_dict["thresh_stable"] = THRESH_STABLE
-param_dict["use_mpc_every"] = USE_MPC_EVERY
-param_dict["speed_factor"] = SPEED_FACTOR
+        with open(
+            os.path.join(self.save_path, "param_dict.json"), "w"
+        ) as outfile:
+            json.dump(self.param_dict, outfile)
 
-with open(os.path.join(SAVE, "param_dict.json"), "w") as outfile:
-    json.dump(param_dict, outfile)
+        # init dataset
+        self.state_data = DroneDataset(
+            self.epoch_size, self.self_play, **self.param_dict
+        )
+        # Init train loader
+        self.trainloader = torch.utils.data.DataLoader(
+            trainer.state_data,
+            batch_size=trainer.batch_size,
+            shuffle=True,
+            num_workers=0
+        )
+        # init optimizer and torch normalization parameters
+        self.optimizer_controller = optim.SGD(
+            self.net.parameters(),
+            lr=self.learning_rate_controller,
+            momentum=0.9
+        )
+        self.optimizer_dynamics = optim.SGD(
+            self.train_dynamics.parameters(),
+            lr=self.learning_rate_dynamics,
+            momentum=0.9
+        )
 
-# init dataset
-state_data = DroneDataset(EPOCH_SIZE, SELF_PLAY, **param_dict)
-# Init train loader
-trainloader = torch.utils.data.DataLoader(
-    state_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
-)
+    def train_controller_model(self, current_state, action_seq, ref_states):
+        # zero the parameter gradients
+        self.optimizer_controller.zero_grad()
+        # save the reached states
+        intermediate_states = torch.zeros(
+            current_state.size()[0], self.nr_actions, self.state_size
+        )
+        for k in range(self.nr_actions):
+            # extract action
+            action = action_seq[:, k]
+            current_state = self.train_dynamics.simulate_quadrotor(
+                action, current_state, dt=self.delta_t
+            )
+            intermediate_states[:, k] = current_state
 
-loss_list, success_mean_list, success_std_list = list(), list(), list()
+        loss = simply_last_loss(
+            intermediate_states, ref_states[:, -1], action_seq, printout=0
+        )
 
-take_every_x = 10
-highest_success = 0  #  np.inf
-for epoch in range(NR_EPOCHS):
+        # Backprop
+        loss.backward()
+        self.optimizer_controller.step()
+        return loss
 
-    try:
+    def train_dynamics_model(self, current_state, action_seq):
+        # zero the parameter gradients
+        self.optimizer_dynamics.zero_grad()
+        next_state_d1 = self.train_dynamics.simulate_quadrotor(
+            action_seq[:, 0], current_state, dt=self.delta_t
+        )
+        next_state_d2 = self.eval_dynamics.simulate_quadrotor(
+            action_seq[:, 0], current_state, dt=self.delta_t
+        )
+        # TODO: weighting --> now velocity much more than attitude etc
+        loss = torch.sum((next_state_d1 - next_state_d2)**2)
+        # print(train_dynamics.mass.grad)
+        # print(train_dynamics.down_drag.grad)
+        # print(train_dynamics.torch_inertia_vector.grad)
+        # print(train_dynamics.torch_kinv_vector.grad)
+        loss.backward()
+        self.optimizer_dynamics.step()
+        return loss
+
+    def run_epoch(self, train="controller"):
+        # tic_epoch = time.time()
+        running_loss = 0
+        for i, data in enumerate(self.trainloader, 0):
+            # inputs are normalized states, current state is unnormalized in
+            # order to correctly apply the action
+            in_state, current_state, in_ref_state, ref_states = data
+
+            # ------------ VERSION 1 (x states at once)-----------------
+            actions = self.net(in_state, in_ref_state)
+            actions = torch.sigmoid(actions)
+            action_seq = torch.reshape(
+                actions, (-1, self.nr_actions, self.action_dim)
+            )
+
+            if train == "controller":
+                loss = trainer.train_controller_model(
+                    current_state, action_seq, ref_states
+                )
+            else:
+                loss = trainer.train_dynamics_model(current_state, action_seq)
+                self.count_finetune_data += len(current_state)
+
+            running_loss += loss.item()
+        # time_epoch = time.time() - tic
+        return running_loss / i
+
+    def evaluate_model(self, epoch, highest_success):
         # EVALUATE
         print(f"Epoch {epoch} (before)")
-        controller = NetworkWrapper(net, state_data, **param_dict)
-        # specify dynamics to collect more data (exploration)
-        if DYNAMICS == "real_flightmare":
-            eval_env = FlightmareWrapper(param_dict["dt"])
+        controller = NetworkWrapper(
+            self.net, self.state_data, **self.param_dict
+        )
+        # specify self.dynamics to collect more data (exploration)
+        if self.dynamics == "real_flightmare":
+            eval_env = FlightmareWrapper(self.param_dict["dt"])
         else:
-            eval_env = QuadRotorEnvBase(EvalDynamics, param_dict["dt"])
+            eval_env = QuadRotorEnvBase(
+                self.eval_dynamics, self.param_dict["dt"]
+            )
 
-        evaluator = QuadEvaluator(controller, eval_env, **param_dict)
+        evaluator = QuadEvaluator(controller, eval_env, **self.param_dict)
         # run with mpc to collect data
         # eval_env.run_mpc_ref("rand", nr_test=5, max_steps=500)
         # run without mpc for evaluation
         suc_mean, suc_std = evaluator.eval_ref(
-            "rand", nr_test=10, max_steps=MAX_STEPS, **param_dict
+            "rand", nr_test=10, max_steps=self.max_steps, **self.param_dict
         )
-
-        success_mean_list.append(suc_mean)
-        success_std_list.append(suc_std)
 
         if (epoch + 1) % 3 == 0:
             # renew the sampled data
-            state_data.resample_data()
-            print(f"Sampled new data ({state_data.num_sampled_states})")
-        print(f"self play counter: {state_data.eval_counter}")
+            self.state_data.resample_data()
+            print(f"Sampled new data ({self.state_data.num_sampled_states})")
 
-        if epoch % 5 == 0 and param_dict["thresh_div"] < THRESH_DIV_END:
-            param_dict["thresh_div"] += .05
-            print("increased thresh div", param_dict["thresh_div"])
+        if epoch % 5 == 0 and self.param_dict["thresh_div"
+                                              ] < self.thresh_div_end:
+            self.param_dict["thresh_div"] += .05
+            print("increased thresh div", self.param_dict["thresh_div"])
 
         # save best model
         if epoch > 0 and suc_mean > highest_success:
             highest_success = suc_mean
             print("Best model")
-            torch.save(net, os.path.join(SAVE, "model_quad" + str(epoch)))
+            torch.save(
+                self.net,
+                os.path.join(self.save_path, "model_quad" + str(epoch))
+            )
 
+        return suc_mean, suc_std
+
+    def finalize(self, mean_list, std_list, losses):
+        torch.save(self.net, os.path.join(self.save_path, "model_quad"))
+        plot_loss_episode_len(
+            mean_list,
+            std_list,
+            losses,
+            save_path=os.path.join(self.save_path, "performance.png")
+        )
+        print("finished and saved.")
+
+
+if __name__ == "__main__":
+
+    nr_epochs = 200
+    train_model_every = 2
+
+    trainer = TrainDrone()
+    base_model = "trained_models/drone/baseline_flightmare"
+
+    trainer.initialize_model(base_model)
+
+    loss_list, success_mean_list, success_std_list = list(), list(), list()
+
+    highest_success = 0  #  np.inf
+    for epoch in range(nr_epochs):
+        model_to_train = (
+            "controller" if
+            (epoch + 1) % train_model_every == 0 else "dynamics"
+        )
+
+        # EVALUATE
+        suc_mean, suc_std = trainer.evaluate_model(epoch, highest_success)
+        success_mean_list.append(suc_mean)
+        success_std_list.append(suc_std)
+
+        print("Counter of data to fine tune:", trainer.count_finetune_data)
+        # print("Params of dynamics model:")
+        # for param in trainer.train_dynamics.parameters():
+        #     print(param.data)
         print()
 
-        # Training
-        tic_epoch = time.time()
-        running_loss = 0
+        # RUN training
+        epoch_loss = trainer.run_epoch(train=model_to_train)
 
-        for i, data in enumerate(trainloader, 0):
-            # inputs are normalized states, current state is unnormalized in
-            # order to correctly apply the action
-            in_state, current_state, in_ref_state, ref_states = data
+        loss_list.append(epoch_loss)
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+        print(f"Loss ({model_to_train}): {round(epoch_loss, 2)}")
+        # print("time one epoch", time.time() - tic_epoch)
 
-            # ------------ VERSION 1 (x states at once)-----------------
-            actions = net(in_state, in_ref_state)
-            actions = torch.sigmoid(actions)
-            action_seq = torch.reshape(actions, (-1, NR_ACTIONS, ACTION_DIM))
-            # save the reached states
-            intermediate_states = torch.zeros(
-                in_state.size()[0], NR_ACTIONS, STATE_SIZE
-            )
-            for k in range(NR_ACTIONS):
-                # extract action
-                action = action_seq[:, k]
-                current_state = TrainDynamics.simulate_quadrotor(
-                    action, current_state, dt=DELTA_T
-                )
-                intermediate_states[:, k] = current_state
-
-            # print_state_ref_div(
-            #     intermediate_states[0].detach().numpy(),
-            #     ref_states[0].detach().numpy()
-            # )
-            # exit()
-
-            loss = simply_last_loss(
-                intermediate_states, ref_states[:, -1], action_seq, printout=0
-            )
-
-            # Backprop
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-
-        loss_list.append(running_loss / i)
-
-    except KeyboardInterrupt:
-        break
-    print("Loss:", round(running_loss / i, 2))
-    # print("time one epoch", time.time() - tic_epoch)
-
-if not os.path.exists(SAVE):
-    os.makedirs(SAVE)
-
-# Save model
-torch.save(net, os.path.join(SAVE, "model_quad"))
-plot_loss_episode_len(
-    success_mean_list,
-    success_std_list,
-    loss_list,
-    save_path=os.path.join(SAVE, "performance.png")
-)
-print("finished and saved.")
+    # Save model
+    trainer.finalize(success_mean_list, success_std_list, loss_list)
